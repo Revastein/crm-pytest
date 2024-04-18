38e7733fef040252562a083f3ef5334b7b3920c4
YaSKovzel <y.kovzel.s@gmail.com>
2024-04-18 08:59:23 +0400

    Add login_page.py for defining LoginPage class with login functionality

---
+++ pages/login_page.py
@@ -0,0 +1,25 @@
+import allure
+
+from base.base_class import BaseClass
+from base.base_page import BasePage
+from config.links import Links
+
+
+class LoginPage(BasePage, BaseClass):
+    page_url = Links.LOGIN_PAGE
+
+    username_field = "//input[@name='username']"
+    password_field = "//input[@name='password']"
+    submit_button = "//button[@type='submit']"
+
+    @allure.step("Enter login")
+    def enter_login(self, login):
+        self.is_clickable("xpath", self.username_field).send_keys(login)
+
+    @allure.step("Enter password")
+    def enter_password(self, password):
+        self.is_clickable("xpath", self.password_field).send_keys(password)
+
+    @allure.step("Click submit button")
+    def click_submit_button(self):
+        self.is_clickable("xpath", self.submit_button).click()
