9ea1e57002c7fcc546c7346856d26831f3492d6e
YaSKovzel <y.kovzel.s@gmail.com>
2024-04-18 09:00:06 +0400

    Add dashboard_page.py for defining DashboardPage class with navigation functionality

---
+++ pages/dashboard_page.py
@@ -0,0 +1,20 @@
+import allure
+
+from base.base_class import BaseClass
+from base.base_page import BasePage
+from config.links import Links
+
+
+class DashboardPage(BasePage, BaseClass):
+    page_url = Links.DASHBOARD_PAGE
+
+    my_info_button = "//span[text()='My Info']"
+    admin_button = "//span[text()='Admin']"
+
+    @allure.step("Click on 'My Info' button'")
+    def click_my_info_button(self):
+        self.is_clickable("xpath", self.my_info_button).click()
+
+    @allure.step("Click on 'Admin' button")
+    def click_admin_button(self):
+        self.is_clickable("xpath", self.admin_button).click()
